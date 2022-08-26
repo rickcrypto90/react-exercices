31daa647de7f2c08533d5f464e992aa2284d8ef7
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-08-26 17:07:19 +0200

    done: props-01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hello } from './Hello';
+import {Welcome} from "./Welcome";
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <Hello />
+                <Welcome name="Rick" />
             </div>)
     }
 }
