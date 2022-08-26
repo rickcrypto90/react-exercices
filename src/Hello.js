ad62b6e7b86dea3371aa14275a47b126adaea706
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-08-26 16:58:22 +0200

    done: components-06

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,11 @@
 import React from "react";
 export class Hello extends React.Component {
     render() {
-        return(
-        <h1>Hello, World!</h1>)
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <p>What a beautiful day!</p>
+            </div>
+        );
     }
 }
