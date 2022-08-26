83dd2cdfdb053268b6252c2b08b58b8247dc462b
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-08-26 16:45:25 +0200

    done: components-03

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Hello } from "./Hello";
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Hello />
+            </div>)
+    }
+}
