f28f7cfe89371941d11bc447c33ebe5984b6b3c6
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-09-09 19:22:01 +0200

    exercise done

--- src/App.js
+++ src/App.js
@@ -3,37 +3,38 @@ import { Hello } from './Hello';
 import { Welcome } from "./Welcome";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Counter } from "./Counter";
-import {ClickCounter } from "./ClickCounter";
-import {ClickTracker } from "./ClickTracker";
+import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from "./ClickTracker";
 import { Login } from "./Login";
 import { LoginUncontrolled } from "./LoginUncontrolled";
 import { TodoList } from "./TodoList";
+import { Container } from "./Container";
 export class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {login: false}
-        this.onLogin= this.onLogin.bind(this);}
+        this.state = { login: false }
+        this.onLogin = this.onLogin.bind(this);
+    }
 
-        onLogin(e){
-            e.preventDefault();
-            this.setState({
-                login: true});
-        }
+    onLogin(e) {
+        e.preventDefault();
+        this.setState({
+            login: true
+        });
+    }
     render() {
         return (
-            <div>
+            <Container>
                 <Hello />
                 <Welcome name="John" age="17" />
-                <Counter count="0" increment="1" interval="1000"/>
-                <ClickCounter/>
-                <ClickTracker/>
-                <InteractiveWelcome/>
-                <Login onLogin={this.onLogin} login={this.state.login}/>
+                <Counter count="0" increment="1" interval="1000" />
+                <ClickCounter />
+                <ClickTracker />
+                <InteractiveWelcome />
+                <Login onLogin={this.onLogin} login={this.state.login} />
                 <br />
-                <LoginUncontrolled/>
-                <TodoList/>
-                
-
-            </div>)
+                <LoginUncontrolled />
+                <TodoList />
+            </Container>)
     }
 }
