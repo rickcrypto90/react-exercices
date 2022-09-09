bafd021718148e696932035a8ef1c802e4062ef4
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-09-09 18:39:28 +0200

    exercise done

--- src/Login.js
+++ src/Login.js
@@ -40,6 +40,7 @@ export class Login extends React.Component {
 
     render() {
         const disabled = (this.state.username && this.state.password) ? false : true
+        const classButton = this.state.password.length <8 ? "red" : "green"
         return (
             <div className="login">
                 <div className="form-group">
@@ -53,7 +54,7 @@ export class Login extends React.Component {
                             <input type="password" name="password" value={this.state.password} onChange={this.handleInput} /><br /> <br />
                             <label>Remember me?</label>
                             <input type="checkbox" name="remember" value={this.state.remember} onChange={this.handleInput} /><br /><br />
-                            <input type="submit" disabled={disabled} onClick={this.onLogin} /><br /><br />
+                            <input type="submit" disabled={disabled} onClick={this.onLogin} className= {classButton} /><br /><br />
                             <button onClick={this.handleReset}>Reset</button>
                             <p>State: {JSON.stringify(this.state)}</p>
     
