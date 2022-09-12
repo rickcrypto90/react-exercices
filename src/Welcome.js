40e2164e1fffae090048dc0996eef2e802ad740e
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-09-12 16:59:04 +0200

    exercise done

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Age } from './Age'
-export class Welcome extends React.Component {
-    render() {
-        return (
-            (<div className="welcome">
-                <p>Welcome <strong>{this.props.name}</strong></p>
-                {(this.props.age && this.props.age < 65 && this.props.name === "John") && <Age age={this.props.age} />}
-            </div>)
-        )
-    }
+// export class Welcome extends React.Component {
+//     render() {
+//         return (
+//             (<div className="welcome">
+//                 <p>Welcome <strong>{props.name}</strong></p>
+//                 {(props.age && props.age < 65 && props.name === "John") && <Age age={props.age} />}
+//             </div>)
+//         )
+//     }
+// }
+// Welcome.defaultProps = {
+//     name: "Stranger"
+// }
+
+
+export function Welcome(props) {
+    return(
+        <div className="welcome">
+                <p>Welcome <strong>{props.name}</strong></p>
+                {(props.age && props.age < 65 && props.name === "John") && <Age age={props.age} />}
+            </div>
+    )
 }
 Welcome.defaultProps = {
     name: "Stranger"
