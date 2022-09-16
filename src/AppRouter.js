9d30243301ccdab3c335f0f6f9298d5eebcab073
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-09-17 00:42:21 +0200

    done

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Welcome } from "./Welcome";
 import {Counter} from "./Counter";
 import {Route, Routes} from "react-router-dom";
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export  function AppRouter() {
     return (
         <Routes>
           <Route path="/" element={<Welcome name="John" />}/>
           <Route path="/counter" element={<Counter />}/>
+          <Route path="/users/:username" element={<ShowGithubUser />}/>
+          
         </Routes>
  
     );
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 export function useGithubUser(username) {
         const [user, setUser] = useState("")
