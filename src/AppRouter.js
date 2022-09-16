aedde57857aaabe9f95f989f4d9253aff5e972e4
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-09-16 18:19:09 +0200

    done

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,6 +5,13 @@ import {ShowGithubUser} from "./ShowGithubUser";
 import { BrowserRouter, Route, Routes, Link} from "react-router-dom";
 
 export  function AppRouter() {
+    function PageNotFound() {
+        return (
+          <div>
+            <h2>Bro, ritenta. (404) </h2>
+          </div>
+        );
+      }
     return (
       <BrowserRouter>
         <Link to="/">Welcome</Link>
@@ -14,6 +21,7 @@ export  function AppRouter() {
           <Route path="/" element={<Welcome name="John" />}/>
           <Route path="/counter" element={<Counter />}/>
           <Route path="/users/:username" element={<ShowGithubUser/>}/>
+          <Route path="*" element={<PageNotFound />} />
         
         </Routes>
       </BrowserRouter>
