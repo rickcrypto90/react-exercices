410fee9f59264a2b31f4c58df78f209ea906f52a
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-09-17 12:57:20 +0200

    done

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -3,7 +3,7 @@ import useSWR from "swr";
 export function useGithubUser(username) {
         const shouldFetch = username !==null
         const fetcher = url=> fetch(url).then(response => {if(response.ok){ return response.json() } else{throw new Error('There was an error')}}) 
-        const {data, error, isLoading = !data&&!error } = useSWR(shouldFetch ? `https://api.github.com/users/${username}`: null, fetcher)
+        const {data, error, isLoading = !data&&!error } = useSWR(()=> shouldFetch ? `https://api.github.com/users/${username}`: null, fetcher)
         return {
                 data,
                 error,
