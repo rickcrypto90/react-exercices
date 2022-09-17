63ea7d905a567e6da731d94e63c12c477f25b2dd
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-09-17 12:55:03 +0200

    done

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react"
 import useSWR from "swr";
 
 export function useGithubUser(username) {
+        const shouldFetch = username !==null
         const fetcher = url=> fetch(url).then(response => {if(response.ok){ return response.json() } else{throw new Error('There was an error')}}) 
-        const {data, error, isLoading = !data&&!error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+        const {data, error, isLoading = !data&&!error } = useSWR(shouldFetch ? `https://api.github.com/users/${username}`: null, fetcher)
         return {
                 data,
                 error,
